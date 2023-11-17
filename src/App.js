b29b37c8f9955f4730672bfebba0e75ba730c549
truchejude <jude.marzat@epitech.eu>
2023-11-17 07:15:27 +0100

    [USE navigate

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import MainPage from './MainPage'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Info from './Info'
+import { BrowserRouter, Routes, Route, useNavigate} from "react-router-dom";
 import './App.css';
 
 function App() {
+
   return (
-    <div>
-      <BrowserRouter>
+    <BrowserRouter>
+      <div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <div className="conteneur-navbar">
+            <NavBar />
+          </div>
+        </div>
         <div className="container">
           <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/Gelleries" element={<p>CACA</p>} />
+            <Route path="/" element={<MainPage />} />
+            <Route path="/Gelleries" element={<p>Gelleries</p>} />
+            <Route path="/Info" element={<Info />} />
           </Routes>
         </div>
-      </BrowserRouter>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function NavBar() {
+  const navigate = useNavigate();
+
+  function ButtonNavigation() {
+    return (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+          <p onClick={() => navigate('/Info')}>Info</p>
+          <p style={{marginLeft: '20px'}} onClick={() => navigate('/Gelleries')}>Gelleries</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="nev-bar">
+      <h1 onClick={() => navigate('/')} style={{ font: 'Monospace' }}>Hugo Segato</h1>
+      <ButtonNavigation />
     </div>
   );
 }
--- src/MainPage.js
+++ src/MainPage.js
@@ -2,29 +2,10 @@ import React from "react";
 import ImageStar from './imageStar/ImageStar'
 
 function MainPage() {
-  function NavBar() {
-    function ButtonNavigation() {
-      return (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <p><a href="/">Info</a></p>
-          <p><a href="/Gelleries">Gelleries</a></p>
-        </div>
-      )
-    }
 
-    return (
-      <div className="nev-bar">
-        <h1 style={{ font: 'Monospace' }}>Hugo Segato</h1>
-        <ButtonNavigation />
-      </div>
-    )
-  }
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', minHeight: '90vh' }}>
-      <div className="conteneur-navbar">
-        <NavBar />
-      </div>
       <div className="conteneur-page">
         <div style={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '40px' }}>
           <div style={{ width: '100%', overflow: 'hidden' }}>
