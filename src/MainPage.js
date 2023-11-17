1683e7af263541ffa42d4e4b27262cfd5c685a1f
truchejude <jude.marzat@epitech.eu>
2023-11-17 06:10:02 +0100

    [nothing]

--- src/MainPage.js
+++ src/MainPage.js
@@ -34,7 +34,7 @@ function MainPage() {
       </div>
       <footer>
         <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-          © HUGO SEGATO - all Rights reserved
+          v0.03 © HUGO SEGATO - all Rights reserved
         </div>
       </footer>
     </div>
