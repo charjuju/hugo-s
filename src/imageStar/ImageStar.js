505599d42ab6633af2af37cc179412e9933c15ed
truchejude <jude.marzat@epitech.eu>
2023-11-18 00:21:34 +0100

    [UP phone

--- src/imageStar/ImageStar.js
+++ src/imageStar/ImageStar.js
@@ -12,26 +12,50 @@ import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 function ImageStar() {
   return (
     <div>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2000,
-          disableOnInteraction: false,
-          pauseOnMouseEnter: true,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-      >
-        <SwiperSlide><img style={{width: '100%'}} src="imageStar/broum.jpeg" alt="imageStar"/></SwiperSlide>
-        <SwiperSlide><img style={{width: '100%'}} src="imageStar/drift.jpeg" alt="imageStar"/></SwiperSlide>
-        <SwiperSlide><img style={{width: '100%'}} src="imageStar/broum.jpeg" alt="imageStar"/></SwiperSlide>
-        <SwiperSlide><img style={{width: '100%'}} src="imageStar/drift.jpeg" alt="imageStar"/></SwiperSlide>
-      </Swiper>
+      <div className="pc-display">
+        <div>
+          <Swiper
+            spaceBetween={30}
+            centeredSlides={true}
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Autoplay, Pagination, Navigation]}
+          >
+            <SwiperSlide><img style={{ width: '100%' }} src="imageStar/broum.jpeg" alt="imageStar" /></SwiperSlide>
+            <SwiperSlide><img style={{ width: '100%' }} src="imageStar/drift.jpeg" alt="imageStar" /></SwiperSlide>
+            <SwiperSlide><img style={{ width: '100%' }} src="imageStar/broum.jpeg" alt="imageStar" /></SwiperSlide>
+            <SwiperSlide><img style={{ width: '100%' }} src="imageStar/drift.jpeg" alt="imageStar" /></SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+      <div className="phone-display">
+        <div>
+          <Swiper
+            spaceBetween={30}
+            centeredSlides={true}
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            modules={[Autoplay]}
+          >
+            <SwiperSlide><img style={{ width: '100%' }} src="imageStar/broum.jpeg" alt="imageStar" /></SwiperSlide>
+            <SwiperSlide><img style={{ width: '100%' }} src="imageStar/drift.jpeg" alt="imageStar" /></SwiperSlide>
+            <SwiperSlide><img style={{ width: '100%' }} src="imageStar/broum.jpeg" alt="imageStar" /></SwiperSlide>
+            <SwiperSlide><img style={{ width: '100%' }} src="imageStar/drift.jpeg" alt="imageStar" /></SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
     </div>
+
   )
 }
 
